0d1a09bf10a43d757c65c15a89d28dd599b6647c
kalaksi <kalaksi@users.noreply.github.com>
2022-06-28 00:56:45 +0300

    Add a theme for testing

--- src/frontend/qt/resources.rs
+++ src/frontend/qt/resources.rs
@@ -1,7 +1,7 @@
 use qmetaobject::qrc;
 
 pub fn init_resources() {
-    qrc!(resources, 
+    qrc!(resources_main, 
         "src/frontend/qt" as "main" {
             "fonts/Pixeloid/PixeloidSans-nR3g1.ttf" as "fonts/pixeloid",
             "fonts/PressStart2P/PressStart2P-vaV7.ttf" as "fonts/pressstart2p",
@@ -15,8 +15,30 @@ pub fn init_resources() {
             "images/fontawesome/circle-arrow-up.svg" as "images/status/up",
             "images/fontawesome/circle-arrow-down.svg" as "images/status/down",
             "images/fontawesome/circle-question.svg" as "images/status/unknown",
+
+            "images/fontawesome/chevron-down.svg" as "images/button/chevron-down"
         }
     );
 
-    resources();
+    qrc!(resources_theme1, 
+        "src/frontend/qt" as "main" {
+            "fonts/Pixeloid/PixeloidSans-nR3g1.ttf" as "fonts/pixeloid",
+            "fonts/PressStart2P/PressStart2P-vaV7.ttf" as "fonts/pressstart2p",
+
+            "images/fontawesome/circle-exclamation.svg" as "images/criticality/critical",
+            "images/fontawesome/circle-exclamation.svg" as "images/criticality/error",
+            "images/fontawesome/circle-exclamation.svg" as "images/criticality/warning",
+            "images/fontawesome/circle-check.svg" as "images/criticality/normal",
+            "images/fontawesome/circle-question.svg" as "images/criticality/unknown",
+
+            "images/fontawesome/check.svg" as "images/status/up",
+            "images/fontawesome/exclamation.svg" as "images/status/down",
+            "images/fontawesome/question.svg" as "images/status/unknown",
+
+            "images/fontawesome/chevron-down.svg" as "images/button/chevron-down"
+        }
+    );
+
+    resources_main();
+    resources_theme1();
 }
