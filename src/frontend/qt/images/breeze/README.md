adea4913cc7240ff2f35185cd7c79a2162b454a0
kalaksi <kalaksi@users.noreply.github.com>
2022-07-03 16:37:21 +0300

    Add a refresh icon from breeze theme

---
+++ src/frontend/qt/images/breeze/README.md
@@ -0,0 +1,1 @@
+Breeze icons copyright KDE and licenced under the GNU LGPL version 3 or later (https://develop.kde.org/frameworks/breeze-icons/).
--- src/frontend/qt/resources.rs
+++ src/frontend/qt/resources.rs
@@ -16,11 +16,12 @@ pub fn init_resources() {
             "images/fontawesome/circle-arrow-down.svg" as "images/status/down",
             "images/fontawesome/circle-question.svg" as "images/status/unknown",
 
-            "images/fontawesome/chevron-down.svg" as "images/button/chevron-down"
+            "images/breeze/dark/view-refresh.svg" as "images/button/refresh",
         }
     );
+    resources_main();
 
-    qrc!(resources_theme1, 
+    qrc!(resources_theme_light, 
         "src/frontend/qt" as "main" {
             "fonts/Pixeloid/PixeloidSans-nR3g1.ttf" as "fonts/pixeloid",
             "fonts/PressStart2P/PressStart2P-vaV7.ttf" as "fonts/pressstart2p",
@@ -31,14 +32,33 @@ pub fn init_resources() {
             "images/fontawesome/circle-check.svg" as "images/criticality/normal",
             "images/fontawesome/circle-question.svg" as "images/criticality/unknown",
 
-            "images/fontawesome/check.svg" as "images/status/up",
-            "images/fontawesome/exclamation.svg" as "images/status/down",
-            "images/fontawesome/question.svg" as "images/status/unknown",
+            "images/fontawesome/circle-arrow-up.svg" as "images/status/up",
+            "images/fontawesome/circle-arrow-down.svg" as "images/status/down",
+            "images/fontawesome/circle-question.svg" as "images/status/unknown",
 
-            "images/fontawesome/chevron-down.svg" as "images/button/chevron-down"
+            "images/breeze/light/view-refresh.svg" as "images/button/refresh",
         }
     );
+    resources_theme_light();
 
-    resources_main();
-    resources_theme1();
+    // qrc!(resources_theme1, 
+    //     "src/frontend/qt" as "main" {
+    //         "fonts/Pixeloid/PixeloidSans-nR3g1.ttf" as "fonts/pixeloid",
+    //         "fonts/PressStart2P/PressStart2P-vaV7.ttf" as "fonts/pressstart2p",
+
+    //         "images/fontawesome/circle-exclamation.svg" as "images/criticality/critical",
+    //         "images/fontawesome/circle-exclamation.svg" as "images/criticality/error",
+    //         "images/fontawesome/circle-exclamation.svg" as "images/criticality/warning",
+    //         "images/fontawesome/circle-check.svg" as "images/criticality/normal",
+    //         "images/fontawesome/circle-question.svg" as "images/criticality/unknown",
+
+    //         "images/fontawesome/check.svg" as "images/status/up",
+    //         "images/fontawesome/exclamation.svg" as "images/status/down",
+    //         "images/fontawesome/question.svg" as "images/status/unknown",
+
+    //         "images/fontawesome/chevron-down.svg" as "images/button/chevron-down"
+    //     }
+    // );
+    // resources_theme1();
+
 }
