fa7a27857b59c9276295ebb1c70ef1f158052722
kalaksi <kalaksi@users.noreply.github.com>
2023-04-12 20:10:06 +0300

    Add a tux icon

--- src/frontend/qt/resources.rs
+++ src/frontend/qt/resources.rs
@@ -41,6 +41,9 @@ pub fn init_resources() {
             "images/breeze/dark/dialog-ok.svg" as "images/button/ok",
             "images/breeze/dark/dialog-cancel.svg" as "images/button/cancel",
 
+            // Host icons
+            "images/breeze/light/preferences-system-linux.svg" as "images/host/linux",
+
             // Category icons
             "images/fontawesome/docker.svg" as "images/docker",
 
@@ -98,6 +101,9 @@ pub fn init_resources() {
             "images/breeze/light/overflow-menu.svg" as "images/button/overflow-menu",
             "images/breeze/light/dialog-ok.svg" as "images/button/ok",
             "images/breeze/light/dialog-cancel.svg" as "images/button/cancel",
+
+            // Host icons
+            "images/breeze/light/preferences-system-linux.svg" as "images/host/linux",
 
             // Category icons
             "images/fontawesome/docker.svg" as "images/docker",
