f7c6af53c6b67b3d188c4fcac84a05ae5d84e4e8
kalaksi <kalaksi@users.noreply.github.com>
2023-04-18 19:30:12 +0300

    Loosen the ConnectionModule trait requirements

--- src/module/connection/connection_module.rs
+++ src/module/connection/connection_module.rs
@@ -9,25 +9,38 @@ use crate::module::connection::ResponseMessage;
 pub type Connector = Box<dyn ConnectionModule + Send>;
 
 pub trait ConnectionModule : MetadataSupport + Module {
-    /// Connect to the specified address. Should do nothing if already connected.
-    fn connect(&mut self, address: &IpAddr) -> Result<(), String>;
-
     /// Send message over the established connection.
     fn send_message(&mut self, message: &str) -> Result<ResponseMessage, String>;
 
-    fn download_file(&self, source: &String) -> io::Result<Vec<u8>>;
-    fn upload_file(&self, destination: &String, contents: Vec<u8>) -> io::Result<()>;
+    fn download_file(&self, _source: &String) -> io::Result<Vec<u8>> {
+        Err(io::Error::new(io::ErrorKind::Other, "Not implemented"))
+    }
+
+    fn upload_file(&self, _destination: &String, _contents: Vec<u8>) -> io::Result<()> {
+        Err(io::Error::new(io::ErrorKind::Other, "Not implemented"))
+    }
 
     /// Check the connection status. Only relevant to modules that use a persistent connection.
     fn is_connected(&self) -> bool {
         false
     }
 
-    fn disconnect(&mut self);
-
-    fn reconnect(&mut self) -> Result<(), String>;
-
     fn new_connection_module(settings: &HashMap<String, String>) -> Connector where Self: Sized + 'static + Send {
         Box::new(Self::new(settings))
+    }
+
+    // These are only relevant to modules that use a persistent connection.
+
+    /// Connect to the specified address. Should do nothing if already connected.
+    fn connect(&mut self, _address: &IpAddr) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn disconnect(&mut self) {
+        // Do nothing by default.
+    }
+
+    fn reconnect(&mut self) -> Result<(), String> {
+        Ok(())
     }
 }
