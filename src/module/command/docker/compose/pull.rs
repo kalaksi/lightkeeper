11299febb6fb9248fc3645a8e9043c043adeae25
kalaksi <kalaksi@users.noreply.github.com>
2023-06-24 22:24:44 +0300

    Improve docker-compose command platform support

--- src/module/command/docker/compose/pull.rs
+++ src/module/command/docker/compose/pull.rs
@@ -36,22 +36,31 @@ impl CommandModule for Pull {
     }
 
     fn get_connector_message(&self, host: Host, parameters: Vec<String>) -> Result<String, String> {
+        let compose_file = parameters.first().unwrap();
+
         let mut command = ShellCommand::new();
+        command.use_sudo = host.settings.contains(&crate::host::HostSetting::UseSudo);
 
-        if host.platform.os == platform_info::OperatingSystem::Linux {
-            let compose_file = parameters.first().unwrap();
+        if host.platform.version_is_same_or_greater_than(platform_info::Flavor::Debian, "8") ||
+           host.platform.version_is_same_or_greater_than(platform_info::Flavor::Ubuntu, "20") {
+
             command.arguments(vec!["docker-compose", "-f", compose_file, "pull"]);
-
             if let Some(service_name) = parameters.get(1) {
                 command.argument(service_name);
             }
+        }
+        else if host.platform.version_is_same_or_greater_than(platform_info::Flavor::RedHat, "8") ||
+                host.platform.version_is_same_or_greater_than(platform_info::Flavor::CentOS, "8") {
 
-            command.use_sudo = host.settings.contains(&crate::host::HostSetting::UseSudo);
-            Ok(command.to_string())
+            command.arguments(vec!["docker", "compose", "-f", compose_file, "pull"]);
+            if let Some(service_name) = parameters.get(1) {
+                command.argument(service_name);
+            }
         }
         else {
-            Err(String::from("Unsupported platform"))
+            return Err(String::from("Unsupported platform"));
         }
+        Ok(command.to_string())
     }
 
     fn process_response(&self, _host: Host, response: &connection::ResponseMessage) -> Result<CommandResult, String> {
--- src/module/command/docker/compose/stop.rs
+++ src/module/command/docker/compose/stop.rs
@@ -45,18 +45,26 @@ impl CommandModule for Stop {
         let mut command = ShellCommand::new();
         command.use_sudo = host.settings.contains(&crate::host::HostSetting::UseSudo);
 
-        if host.platform.os == platform_info::OperatingSystem::Linux {
-            command.arguments(vec!["docker-compose", "-f", compose_file, "stop"]);
+        if host.platform.version_is_same_or_greater_than(platform_info::Flavor::Debian, "8") ||
+           host.platform.version_is_same_or_greater_than(platform_info::Flavor::Ubuntu, "20") {
 
+            command.arguments(vec!["docker-compose", "-f", compose_file, "stop"]);
             if let Some(service_name) = parameters.get(1) {
                 command.argument(service_name);
             }
+        }
+        else if host.platform.version_is_same_or_greater_than(platform_info::Flavor::RedHat, "8") ||
+                host.platform.version_is_same_or_greater_than(platform_info::Flavor::CentOS, "8") {
 
-            Ok(command.to_string())
+            command.arguments(vec!["docker", "compose", "-f", compose_file, "stop"]);
+            if let Some(service_name) = parameters.get(1) {
+                command.argument(service_name);
+            }
         }
         else {
-            Err(String::from("Unsupported platform"))
+            return Err(String::from("Unsupported platform"));
         }
+        Ok(command.to_string())
     }
 
     fn process_response(&self, _host: Host, response: &connection::ResponseMessage) -> Result<CommandResult, String> {
