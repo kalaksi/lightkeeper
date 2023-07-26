6b9b886c04c4817244c2eb6b742e7700b776b106
kalaksi <kalaksi@users.noreply.github.com>
2023-07-26 23:24:13 +0300

    Rename configuration templates to groups

--- src/configuration.rs
+++ src/configuration.rs
@@ -8,7 +8,7 @@ use crate::file_handler;
 
 const MAIN_CONFIG_FILE: &str = "config.yml";
 const HOSTS_FILE: &str = "hosts.yml";
-const TEMPLATES_FILE: &str = "templates.yml";
+const GROUPS_FILE: &str = "groups.yml";
 
 
 #[derive(Serialize, Deserialize, Default, Clone)]
@@ -21,8 +21,8 @@ pub struct Configuration {
 
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(deny_unknown_fields)]
-pub struct Templates {
-    pub templates: HashMap<String, HostSettings>,
+pub struct Groups {
+    pub groups: HashMap<String, HostSettings>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -82,7 +82,7 @@ pub struct Category {
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct HostSettings {
-    pub templates: Option<Vec<String>>,
+    pub groups: Option<Vec<String>>,
     #[serde(default = "HostSettings::default_address")]
     pub address: String,
     #[serde(default = "HostSettings::default_fqdn")]
@@ -175,7 +175,7 @@ impl Configuration {
 
         let main_config_file_path = config_dir.join(MAIN_CONFIG_FILE);
         let hosts_file_path = config_dir.join(HOSTS_FILE);
-        let templates_file_path = config_dir.join(TEMPLATES_FILE);
+        let groups_file_path = config_dir.join(GROUPS_FILE);
 
         // If main configuration is missing, this is probably the first run, so create initial configurations.
         if let Err(_) = fs::metadata(&main_config_file_path) {
@@ -193,49 +193,49 @@ impl Configuration {
         let mut hosts = serde_yaml::from_str::<Hosts>(hosts_contents.as_str())
                                    .map_err(|error| io::Error::new(io::ErrorKind::Other, error.to_string()))?;
 
-        log::info!("Reading template configuration from {}", templates_file_path.display());
-        let templates_contents = fs::read_to_string(templates_file_path)?;
-        let all_templates = serde_yaml::from_str::<Templates>(templates_contents.as_str())
-                                       .map_err(|error| io::Error::new(io::ErrorKind::Other, error.to_string()))?;
+        log::info!("Reading group configuration from {}", groups_file_path.display());
+        let groups_contents = fs::read_to_string(groups_file_path)?;
+        let all_groups = serde_yaml::from_str::<Groups>(groups_contents.as_str())
+                                    .map_err(|error| io::Error::new(io::ErrorKind::Other, error.to_string()))?;
 
-        // Check there are no invalid template references.
-        let invalid_templates = hosts.hosts.values()
-            .filter(|host_config| host_config.templates.is_some())
-            .flat_map(|host_config| host_config.templates.clone().unwrap_or_else(|| Vec::new()))
-            .filter(|template_id| !all_templates.templates.contains_key(template_id))
+        // Check there are no invalid group references.
+        let invalid_groups = hosts.hosts.values()
+            .filter(|host_config| host_config.groups.is_some())
+            .flat_map(|host_config| host_config.groups.clone().unwrap_or_else(|| Vec::new()))
+            .filter(|group_id| !all_groups.groups.contains_key(group_id))
             .collect::<Vec<String>>();
 
-        if !invalid_templates.is_empty() {
-            let error_message = format!("Invalid template references: {}", invalid_templates.join(", "));
+        if !invalid_groups.is_empty() {
+            let error_message = format!("Invalid group references: {}", invalid_groups.join(", "));
             return Err(io::Error::new(io::ErrorKind::Other, error_message));
         }
 
         for (_, host_config) in hosts.hosts.iter_mut() {
-            if let Some(host_template_ids) = host_config.templates.clone() {
-                for template_id in host_template_ids.iter() {
-                    let template_config = all_templates.templates.get(template_id).unwrap();
+            if let Some(host_group_ids) = host_config.groups.clone() {
+                for group_id in host_group_ids.iter() {
+                    let group_config = all_groups.groups.get(group_id).unwrap();
 
                     // Host settings are not merged.
-                    if template_config.settings.is_some() {
-                        host_config.settings = template_config.settings.clone();
+                    if group_config.settings.is_some() {
+                        host_config.settings = group_config.settings.clone();
                     }
 
-                    // Merge templates.
-                    template_config.monitors.iter().for_each(|(monitor_id, new_config)| {
+                    // Merge groups.
+                    group_config.monitors.iter().for_each(|(monitor_id, new_config)| {
                         let mut merged_config = host_config.monitors.get(monitor_id).cloned().unwrap_or(MonitorConfig::default());
                         merged_config.settings.extend(new_config.settings.clone());
                         merged_config.is_critical = new_config.is_critical;
                         host_config.monitors.insert(monitor_id.clone(), merged_config);
                     });
 
-                    template_config.commands.iter().for_each(|(command_id, new_config)| {
+                    group_config.commands.iter().for_each(|(command_id, new_config)| {
                         let mut merged_config = host_config.commands.get(command_id).cloned().unwrap_or(CommandConfig::default());
                         merged_config.settings.extend(new_config.settings.clone());
                         merged_config.version = new_config.version.clone();
                         host_config.commands.insert(command_id.clone(), merged_config);
                     });
 
-                    template_config.connectors.iter().for_each(|(connector_id, new_config)| {
+                    group_config.connectors.iter().for_each(|(connector_id, new_config)| {
                         let mut merged_config = host_config.connectors.get(connector_id).cloned().unwrap_or(ConnectorConfig::default());
                         merged_config.settings.extend(new_config.settings.clone());
                         host_config.connectors.insert(connector_id.clone(), merged_config);
@@ -250,11 +250,11 @@ impl Configuration {
     pub fn write_initial_configuration(config_dir: PathBuf) -> io::Result<()> {
         let default_main_config = include_str!("../config.example.yml");
         let default_hosts_config = include_str!("../hosts.example.yml");
-        let default_templates_config = include_str!("../templates.example.yml");
+        let default_groups_config = include_str!("../groups.example.yml");
 
         let main_config_file_path = config_dir.join(MAIN_CONFIG_FILE);
         let hosts_file_path = config_dir.join(HOSTS_FILE);
-        let templates_file_path = config_dir.join(TEMPLATES_FILE);
+        let groups_file_path = config_dir.join(GROUPS_FILE);
 
         fs::create_dir_all(&config_dir)?;
 
@@ -292,19 +292,19 @@ impl Configuration {
             }
         }
 
-        let templates_config_file = fs::OpenOptions::new().write(true).create_new(true).open(templates_file_path.clone());
-        match templates_config_file {
+        let groups_config_file = fs::OpenOptions::new().write(true).create_new(true).open(groups_file_path.clone());
+        match groups_config_file {
             Ok(mut file) => {
-                if let Err(error) = file.write_all(default_templates_config.as_bytes()) {
-                    let message = format!("Failed to write template configuration file {}: {}", templates_file_path.to_string_lossy(), error);
+                if let Err(error) = file.write_all(default_groups_config.as_bytes()) {
+                    let message = format!("Failed to write group configuration file {}: {}", groups_file_path.to_string_lossy(), error);
                     return Err(io::Error::new(io::ErrorKind::Other, message));
                 }
                 else {
-                    log::info!("Created new template configuration file {}", templates_file_path.to_string_lossy());
+                    log::info!("Created new group configuration file {}", groups_file_path.to_string_lossy());
                 }
             },
             Err(error) => {
-                let message = format!("Failed to create template configuration file {}: {}", templates_file_path.to_string_lossy(), error);
+                let message = format!("Failed to create group configuration file {}: {}", groups_file_path.to_string_lossy(), error);
                 return Err(io::Error::new(io::ErrorKind::Other, message));
             }
         }
