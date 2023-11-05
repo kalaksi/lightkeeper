79d69d7bb7d7f548b48475b56a3dcbbc23e77197
kalaksi <kalaksi@users.noreply.github.com>
2023-11-05 12:58:03 +0200

    Add vagrant commands to run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 set -eu
-RUST_LOG=debug ./target/debug/lightkeeper --config-dir "test"
+
+enabled_vms=("centos7" "centos8" "debian10" "debian11" "ubuntu2004")
+
+for vm in "${enabled_vms[@]}"; do
+    if [ -d "test/$vm" ]; then
+        cd "test/$vm"
+        vagrant up --no-tty
+        cd -
+    fi
+done
+ 
+ RUST_LOG=debug ./target/debug/lightkeeper --config-dir test
+
+for vm in "${enabled_vms[@]}"; do
+    if [ -d "test/$vm" ]; then
+        cd "test/$vm"
+        vagrant halt --no-tty
+        cd -
+    fi
+done
