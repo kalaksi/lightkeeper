9394e559095ac07d9b2b082b23139bf371efbd5f
kalaksi <kalaksi@users.noreply.github.com>
2023-12-06 08:04:06 +0200

    Add some JS helpers for date operations

--- src/frontend/qt/qml/js/Utils.js
+++ src/frontend/qt/qml/js/Utils.js
@@ -10,3 +10,24 @@ function isIpv4OrIpv6Address(address) {
 function sortNumerically(array) {
     return array.sort((a, b) => a - b);
 }
+
+/// Returns time zone in format +HH:MM or -HH:MM
+function formatTimezone(timezoneOffsetString) {
+    let timezoneOffset = parseInt(timezoneOffsetString);
+    let sign = timezoneOffset < 0 ? '-' : '+';
+    let hours = Math.abs(Math.floor(timezoneOffset / 60));
+    let minutes = Math.abs(timezoneOffset % 60);
+    return sign + (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
+}
+
+function getLocalTimezoneISOString(now) {
+    let date = now === undefined ? new Date() : new Date(now);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Months are zero-based
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    return year + '-' + month + '-' + day + 'T' + hours + ':' + minutes + ':' + seconds;
+}
