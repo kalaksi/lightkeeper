554adaaa618e49a75a00c776f33780c247df1c50
kalaksi <kalaksi@users.noreply.github.com>
2024-03-27 22:12:36 +0200

    ram: widen OS support

--- src/module/monitoring/linux/ram.rs
+++ src/module/monitoring/linux/ram.rs
@@ -38,9 +38,7 @@ impl MonitoringModule for Ram {
     }
 
     fn get_connector_message(&self, host: Host, _parent_result: DataPoint) -> Result<String, String> {
-        if host.platform.version_is_same_or_greater_than(platform_info::Flavor::Debian, "10") ||
-           host.platform.version_is_same_or_greater_than(platform_info::Flavor::Ubuntu, "20") ||
-           host.platform.version_is_same_or_greater_than(platform_info::Flavor::CentOS, "7") {
+        if host.platform.os == platform_info::OperatingSystem::Linux {
             Ok(String::from("free -m"))
         }
         else {
@@ -49,10 +47,7 @@ impl MonitoringModule for Ram {
     }
 
     fn process_response(&self, host: Host, response: ResponseMessage, _parent_result: DataPoint) -> Result<DataPoint, String> {
-        if host.platform.version_is_same_or_greater_than(platform_info::Flavor::Debian, "10") ||
-           host.platform.version_is_same_or_greater_than(platform_info::Flavor::Ubuntu, "20") ||
-           host.platform.version_is_same_or_greater_than(platform_info::Flavor::CentOS, "7") {
-
+        if host.platform.os == platform_info::OperatingSystem::Linux {
             let line = response.message.lines().filter(|line| line.contains("Mem:")).collect::<Vec<&str>>();
             let parts = line[0].split_whitespace().collect::<Vec<&str>>();
 
