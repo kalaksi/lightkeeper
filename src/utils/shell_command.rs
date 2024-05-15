f98e0743ee7f2bfecd8244fa77adcda1d112d24a
kalaksi <kalaksi@users.noreply.github.com>
2024-05-15 21:09:24 +0300

    Revert "Add ignore_error to ShellCommand"
    This reverts commit fe35bdd43140162ee633b15c417d1b4518d5406d.

--- src/utils/shell_command.rs
+++ src/utils/shell_command.rs
@@ -7,7 +7,6 @@ pub struct ShellCommand {
     arguments: VecDeque<String>,
     piped_to: VecDeque<Vec<String>>,
     pub ignore_stderr: bool,
-    pub ignore_error: bool,
     pub use_sudo: bool,
 }
 
@@ -17,7 +16,6 @@ impl ShellCommand {
             arguments: VecDeque::new(),
             piped_to: VecDeque::new(),
             ignore_stderr: false,
-            ignore_error: false,
             use_sudo: false,
         }
     }
@@ -92,10 +90,6 @@ impl ToString for ShellCommand {
 
             if self.ignore_stderr {
                 command_string = format!("{} 2>/dev/null", command_string);
-            }
-
-            if self.ignore_error {
-                command_string = format!("{} || true", command_string);
             }
 
             if !self.piped_to.is_empty() {
