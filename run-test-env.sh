74d311bbdd5232a24585429c4f94af0489e9edba
kalaksi <kalaksi@users.noreply.github.com>
2025-02-26 19:02:42 +0200

    Update test env configs

--- run-test-env.sh
+++ run-test-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
-RUST_LOG=debug ./target/debug/lightkeeper --config-dir test-env
-# flatpak run --filesystem=~/git/lightkeeper/test-env io.github.kalaksi.Lightkeeper --config-dir test-env
+# RUST_LOG=debug ./target/debug/lightkeeper --config-dir test-env
+RUST_LOG=debug flatpak run --filesystem=~/git/lightkeeper/test-env io.github.kalaksi.Lightkeeper-local --config-dir test-env
