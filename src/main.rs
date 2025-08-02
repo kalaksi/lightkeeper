952f2e43df4572b37c47e10b73bc22b4f5f194c7
kalaksi <kalaksi@users.noreply.github.com>
2025-08-02 13:57:46 +0300

    Force english as language (affects buttons)

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,9 @@ pub struct Args {
 }
 
 fn main() {
+    // Only english is currently supported.
+    std::env::set_var("LANGUAGE", "en_US");
+
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = Args::parse();
 
