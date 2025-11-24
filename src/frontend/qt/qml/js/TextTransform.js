1ff3f6707e0d6309e694f3351aaa8f6c965eeed9
kalaksi <kalaksi@users.noreply.github.com>
2025-11-25 00:26:07 +0200

    Improve rich text conversions

--- src/frontend/qt/qml/js/TextTransform.js
+++ src/frontend/qt/qml/js/TextTransform.js
@@ -1,13 +1,13 @@
 var ANSI_COLORS = {
-    "30": "black",
-    "31": "red",
-    "32": "green",
-    "33": "yellow",
-    "34": "blue",
-    "35": "magenta",
-    "36": "cyan",
-    "37": "white",
-    "90": "grey",
+    30: "black",
+    31: "red",
+    32: "green",
+    33: "yellow",
+    34: "blue",
+    35: "magenta",
+    36: "cyan",
+    37: "white",
+    90: "grey",
 }
 
 function trimNewline(text) {
@@ -42,28 +42,33 @@ function removeWhitespaces(text) {
 /// This is probably not completely secure so don't use it for anything important
 function escapeHtml(text)
 {
-    return text.replace(/</g, "&lt;")
+    return text.replace(/&/g, "&amp;")
+               .replace(/</g, "&lt;")
                .replace(/>/g, "&gt;")
-               .replace(/&/g, "&amp;")
                .replace(/"/g, "&quot;")
                .replace(/'/g, "&#039;");
 }
 
 /// Parse ANSI color codes and return rich text with appropriate coloring.
 function ansiToRichText(text) {
+    return text.replace(/\x1b\[([\d;]+)m/g, (match, p1) => {
+        let codes = p1.split(";").map(Number)
 
-    return text.replace(/\[1?\;?(1|3[0-7]|90|0)m/g, (match, p1) => {
-        if (p1 in ANSI_COLORS) {
-            return `<span style="color:${ANSI_COLORS[p1]}">`
-        }
-        else if (p1 === "1") {
-            return '<span style="font-weight:bold">'
-        }
-        else if (p1 === "0") {
+        if (codes.includes(0)) {
             return "</span>"
         }
-        else {
-            return match
+
+        let styles = []
+        for (const code of codes) {
+            if (code === 1) styles.push("font-weight:bold");
+            else if (code === 3) styles.push("font-style:italic");
+            else if (code === 4) styles.push("text-decoration:underline");
+            else if (code === 9) styles.push("text-decoration:line-through");
+            else if (code in ANSI_COLORS) {
+                styles.push(`color:${ANSI_COLORS[code]}`)
+            }
         }
+
+        return styles.length > 0 ? `<span style="${styles.join(";")}">` : ""
     })
 }
