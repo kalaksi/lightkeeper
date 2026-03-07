1b947c0a73f7a43d178e33b0be8a21f643cc4fbe
kalaksi <kalaksi@users.noreply.github.com>
2026-03-07 22:23:07 +0200

    Fix do-release.sh overwriting commit ids

--- do-release.sh
+++ do-release.sh
@@ -53,7 +53,7 @@ echo -e "\n* Updating io.github.kalaksi.Lightkeeper.yml..."
 sed -ri 's|(\s+url: https://github.com/kalaksi/lightkeeper/archive/refs/tags/).*|\1'$new_version'.tar.gz|' io.github.kalaksi.Lightkeeper.yml
 new_checksum=$(wget -qO- "https://github.com/kalaksi/lightkeeper/archive/refs/tags/$new_version.tar.gz"|sha256sum|cut -f 1 -d ' ')
 sed -ri '/url: https:\/\/github.com\/kalaksi\/lightkeeper\/archive/{n;s/(\s+sha256: ).*/\1'$new_checksum'/}' io.github.kalaksi.Lightkeeper.yml
-sed -ri '/- name: qml-lighthouse-components/{:a;n;/commit:/s/(commit: ).*/\1'"$qml_lighthouse_components_commit"'/;ba}' io.github.kalaksi.Lightkeeper.yml
+sed -ri '/- name: qml-lighthouse-components/{:a;n;/commit:/{s/(commit: ).*/\1'"$qml_lighthouse_components_commit"'/;b};ba}' io.github.kalaksi.Lightkeeper.yml
 git commit -a -m "Update flatpak manifest for $new_version"
 git push
 
